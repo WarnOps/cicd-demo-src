844d1fffa669adf022c5b73ea7dde590e6f64459
Alan Warnock <alanwarnock@bel-fgk4vd7ynv.home>
2022-07-03 20:50:49 +0100

    test local

--- utils/buildContainerTemplate.sh
+++ utils/buildContainerTemplate.sh
@@ -5,7 +5,6 @@ while getopts "f:g:s:u:r:t:d:" option;
         f ) SRC_FOLDER=${OPTARG};;
         g ) GITHUB_WORKSPACE=${OPTARG};;
         s ) SOURCE_LOCATION=${OPTARG};;
-        u ) AZ_ACR_NAME=${OPTARG};;
         r ) REPOSITORY=${OPTARG};;
         t ) TASKNAME=${OPTARG};;
         d ) DOCKER_FILE=${OPTARG};;
@@ -14,7 +13,6 @@ done
 echo $SRC_FOLDER
 echo $GITHUB_WORKSPACE
 echo $SOURCE_LOCATION
-echo $AZ_ACR_NAME
 echo $REPOSITORY
 echo $TASKNAME
 echo $DOCKER_FILE
@@ -29,18 +27,14 @@ imageTag=$(git log -n 1 --format="%H" -- ".")
 popd
   
 # If the image with the generated tag doesn't already exist, build it.
-if ! az acr repository show -n $AZ_ACR_NAME --image "$REPOSITORY:$imageTag" -o table; then
-    echo No match found. Container will be built.
-    echo Tag for new container: $imageTag
-    az acr build \
-        -r $AZ_ACR_NAME \
-        -t "$REPOSITORY:$imageTag" \
-        -t "$REPOSITORY:latest" \
-        -f "$GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION/$DOCKER_FILE" \
-        $GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION
-else
-    echo "The existing image with tag "$imageTag" is found."
-fi
+echo Tag for new container: $imageTag
+docker build \
+    -t "$REPOSITORY:$imageTag" \
+    -t "$REPOSITORY:latest" \
+    -f "$GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION/$DOCKER_FILE" \
+    $GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION
+
+
 set +x
 echo "setting IMAGE_TAG output for task $TASKNAME"
 echo "##vso[task.setvariable variable=IMAGE_TAG;isOutput=true]$imageTag"
